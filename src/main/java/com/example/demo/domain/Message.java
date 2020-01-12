938a5581cff608bfe8558fc84fd241b82fba929b
VladStarostenko <38181661+VladStarostenko@users.noreply.github.com>
2020-01-12 18:16:02 +0100

    First

---
+++ src/main/java/com/example/demo/domain/Message.java
@@ -0,0 +1,32 @@
+package com.example.demo.domain;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonView;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table
+@ToString(of = {"id", "text"})
+@EqualsAndHashCode(of = {"id"})
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @JsonView(Views.Id.class)
+    private Long id;
+    @JsonView(Views.IdName.class)
+    private String text;
+
+    @Column(updatable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonView(Views.FullMessage.class)
+    private LocalDateTime creationDate;
+
+    public void setCreationDate(LocalDateTime creationDate) {
+        this.creationDate = creationDate;
+    }
+
+}
